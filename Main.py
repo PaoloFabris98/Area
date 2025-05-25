aff186800ccf05dee73b5082a3fbc499d7d78f0c
PaoloFabris98 <f98paolo@gmail.com>
2025-05-25 16:38:14 +0200

    start

---
+++ Main.py
@@ -0,0 +1,21 @@
+baseTriangolo = 10
+altezzaTriangolo = 10
+areaTriangolo = (baseTriangolo * altezzaTriangolo) / 2
+print("Area Triangolo:",areaTriangolo)
+
+baseMaggiore = 10
+baseMinore = 6
+altezza = 4
+areaTrapezio = ((baseMaggiore + baseMinore) * altezza) / 2
+print("Area Trapezio:",areaTrapezio)
+
+pi = 3.14
+raggio = 2
+areaCerchio = ((raggio**2) * pi)
+print("Area Cerchio:",areaCerchio)
+
+
+a = input("Inserisci un numero: ")
+b = 0.621371
+
+print("Miglia:",(int(a)*b))
